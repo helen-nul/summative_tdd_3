8edf029a4faca89b633a1a589075c8d0c787734a
Helen Brasier <brasierhelen@gmail.com>
2026-01-18 17:43:25 +0000

    adds tests

--- test/test_palindrome.py
+++ test/test_palindrome.py
@@ -1,2 +1,11 @@
+from main import is_pali
+
 def test_pytest():
     assert 2+2 == 4
+
+def test_is_pali_happy():
+        assert is_pali("radar") == True
+        assert is_pali("Radar") == True
+
+def test_is_pali_unhappy():
+        assert is_pali("radar1") == False
